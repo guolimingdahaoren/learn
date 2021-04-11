54b9d31c9d1430517b7a8ffb2ac51199e4ffa7f6
xnguolm <guoliming@imdada.cn>
2021-04-12 07:33:07 +0800

    算法

--- 算法/src/两数相加/Solution.java
+++ 算法/src/两数相加/Solution.java
@@ -1,24 +1,35 @@
 package 两数相加;
 
+/**
+ * @Description: 循环条件; root节点; 进位
+ * @Author: guoliming@imdada.cn
+ * @Date: 2021-04-12 07:30
+**/
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode root = new ListNode(0);
+        ListNode node = root;
         int carry = 0;
         while (l1 != null || l2 != null || carry != 0) {
             int val1 = l1 == null ? 0 : l1.val;
             int val2 = l2 == null ? 0 : l2.val;
             int sum = val1 + val2 + carry;
             ListNode currentNode = new ListNode(sum % 10);
-            root.next = currentNode;
-            root = currentNode;
-            l1 = l1.next;
-            l2 = l2.next;
+            node.next = currentNode;
+            node = currentNode;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
             carry = sum / 10;
         }
         return root.next;
     }
 
 }
+
 class ListNode {
     int val;
 
@@ -30,6 +41,7 @@ class ListNode {
     ListNode(int val) {
         this.val = val;
     }
+
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
